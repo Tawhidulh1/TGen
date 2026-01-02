3b27f0aa6841aa1344e572c3476c9b2365cf8f52
Tawhidul Hasan <Tawhidulh1@gmail.com>
2026-01-02 11:44:58 -0500

    removed untested code

--- tgen.ps1
+++
@@ -1,8 +0,0 @@
-# converted from bash
-Write-Host "setting things up..."
-$GROUP_ID = mvn help:evaluate -Dexpression=project.groupId -q -DforceStdout
-$ARTIFACT_ID = mvn help:evaluate -Dexpression=project.artifactId -q -DforceStdout
-$VERSION = mvn help:evaluate -Dexpression=project.version -q -DforceStdout
-Invoke-Expression ((Invoke-WebRequest -Uri "https://sh.jbang.dev").Content + " app install --name tgen $GROUP_ID`:$ARTIFACT_ID`:$VERSION")
-Write-Host "DONE"
-
